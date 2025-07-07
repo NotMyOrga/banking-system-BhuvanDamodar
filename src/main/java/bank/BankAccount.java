196c1881beaa5394d27a04b795da48aa64b35c59
Bhuvan Damodar A <46862696+BhuvanDamodar@users.noreply.github.com>
2025-07-07 15:32:24 +0200

    deposit and withdraw changes

--- src/main/java/bank/BankAccount.java
+++ src/main/java/bank/BankAccount.java
@@ -16,11 +16,13 @@ public class BankAccount {
     }
 
     public void deposit(double amount) {
-        // TODO: Add amount to balance
+        balance += amount;
     }
 
     public boolean withdraw(double amount) {
-        // TODO: Subtract if enough balance; return true/false
+        if (balance >= amount){
+            balance -= amount;
+        }
         return false;
     }
 }
